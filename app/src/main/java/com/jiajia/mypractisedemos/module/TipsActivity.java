b4272faf0c5c326c7c545eb2f6c86b3b141ee16f
Numen-fan <numen.fan@gmail.com>
2022-08-07 00:45:05 +0800

    最近练习 again

--- app/src/main/java/com/jiajia/mypractisedemos/module/TipsActivity.java
+++ app/src/main/java/com/jiajia/mypractisedemos/module/TipsActivity.java
@@ -1,29 +1,105 @@
 package com.jiajia.mypractisedemos.module;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.os.Bundle;
 import android.view.View;
+import android.view.ViewStub;
+import android.view.animation.Animation;
+import android.view.animation.TranslateAnimation;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
+import com.jiajia.mypractisedemos.BaseActivity;
 import com.jiajia.mypractisedemos.R;
-import com.jiajia.mypractisedemos.module.demo.VoipTipsView;
+import com.jiajia.mypractisedemos.module.kotlin.util.LogUtils;
+import com.jiajia.mypractisedemos.module.kotlin.util.ToastUtils;
+import com.jiajia.mypractisedemos.utils.Utils;
 
-public class TipsActivity extends AppCompatActivity {
+public class TipsActivity extends BaseActivity {
 
-    VoipTipsView tipsView;
+    private static final String TAG = "TipsActivity";
 
+
+    View open;
+
+
+    View settingRoot;
+    RadioGroup mRadioGroup;
+
+    RadioButton mRadioBtnOpen;
+    RadioButton mRadioBtnClose;
+    RadioButton mRadioBtnAuto;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_tips);
+    public int getContentResId() {
+        return R.layout.activity_tips;
+    }
 
-        tipsView = findViewById(R.id.tips_view);
+    @Override
+    public void initUI() {
 
-        findViewById(R.id.btn_normal_tips).setOnClickListener(v -> {
-            tipsView.setVisibility(View.VISIBLE);
+        open = findViewById(R.id.tv_open);
+
+    }
+
+    @Override
+    public void initListener() {
+        open.setOnClickListener(v -> openSettingUI());
+    }
+
+    private void openSettingUI() {
+        if (settingRoot == null) {
+            loadSettingView();
+        }
+        openSettingPage();
+    }
+
+    private void  loadSettingView() {
+        ViewStub viewStub = findViewById(R.id.view_stub_setting);
+        viewStub.inflate();
+
+        settingRoot = findViewById(R.id.setting_root);
+
+        mRadioGroup = findViewById(R.id.radio_group);
+
+        mRadioBtnOpen = findViewById(R.id.radio_open);
+        mRadioBtnClose = findViewById(R.id.radio_close);
+        mRadioBtnAuto = findViewById(R.id.radio_auto);
+
+        mRadioBtnAuto.setChecked(true);
+
+        mRadioGroup.setOnCheckedChangeListener((group, checkedId) -> LogUtils.INSTANCE.debug(TAG, "choose " + checkedId));
+
+        mRadioBtnOpen.setOnCheckedChangeListener((buttonView, isChecked) -> {
+            if (isChecked) {
+                ToastUtils.INSTANCE.showToast("open");
+            }
         });
 
+        mRadioBtnClose.setOnCheckedChangeListener((buttonView, isChecked) -> {
+            if (isChecked) {
+                closeSettingUI();
+            }
+        });
 
+        mRadioBtnAuto.setOnCheckedChangeListener((buttonView, isChecked) -> {
+            if (isChecked) {
+                ToastUtils.INSTANCE.showToast("auto");
+                LogUtils.INSTANCE.debug(TAG, "auto");
+            }
+        });
+
+    }
+
+    private void openSettingPage() {
+        settingRoot.setVisibility(View.VISIBLE);
+        Animation animation = new TranslateAnimation(Utils.getFullScreenWidth(), 0, 0, 0);
+        animation.setDuration(200);
+        settingRoot.setAnimation(animation);
+    }
+
+    private void closeSettingUI() {
+        Animation animation = new TranslateAnimation(0, Utils.getFullScreenWidth(), 0, 0);
+        animation.setDuration(200);
+        settingRoot.setAnimation(animation);
+        settingRoot.setVisibility(View.GONE);
     }
 }
