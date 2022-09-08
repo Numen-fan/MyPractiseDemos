37055c2144cf2154a906fdc229e41c49208001da
fanjiajia02 <fanjiajia02@baidu.com>
2022-09-08 20:30:25 +0800

    尝试添加FlutterView

--- app/src/main/java/com/jiajia/mypractisedemos/module/demo/DemoActivity.java
+++ app/src/main/java/com/jiajia/mypractisedemos/module/demo/DemoActivity.java
@@ -28,6 +28,8 @@ import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.Toast;
 
+import io.flutter.embedding.android.FlutterView;
+
 @Route(path = RouteConfig.APP_DEMO_ACTIVITY)
 public class DemoActivity extends AppCompatActivity {
 
@@ -57,15 +59,13 @@ public class DemoActivity extends AppCompatActivity {
         setContentView(R.layout.activity_margin);
 
         try {
-            if (Build.VERSION.SDK_INT >= 21) {
-                View decorView = getWindow().getDecorView();
-                int option = View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                        | View.SYSTEM_UI_FLAG_LAYOUT_STABLE;
-                decorView.setSystemUiVisibility(option);
+            View decorView = getWindow().getDecorView();
+            int option = View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                    | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                    | View.SYSTEM_UI_FLAG_LAYOUT_STABLE;
+            decorView.setSystemUiVisibility(option);
 //                getWindow().setNavigationBarColor(Color.TRANSPARENT);
-                getWindow().setStatusBarColor(Color.TRANSPARENT);
-            }
+            getWindow().setStatusBarColor(Color.TRANSPARENT);
             //只支持6.0以上的系统；
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
                 this.getWindow().getDecorView().setSystemUiVisibility( View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
@@ -172,30 +172,19 @@ public class DemoActivity extends AppCompatActivity {
         });
 
         bigView = findViewById(R.id.big_view);
-        bigView.setOnClickListener(v -> {
-            ToastUtils.INSTANCE.showToast("Click Big View");
-        });
+
+        addFlutterView();
+
 
 
     }
 
-    @Override
-    public void onWindowFocusChanged(boolean hasFocus) {
-        super.onWindowFocusChanged(hasFocus);
-//        if (hasFocus && Build.VERSION.SDK_INT >= 19) {
-//            View decorView = getWindow().getDecorView();
-//            decorView.setSystemUiVisibility(
-//                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-//                            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-//                            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-//                            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-//                            | View.SYSTEM_UI_FLAG_FULLSCREEN
-//                            | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
-//        }
-    }
+    private void addFlutterView() {
 
-    public int dp2px(int dp) {
-        return (int) (dp * Resources.getSystem().getDisplayMetrics().density);
+//        FlutterView flutterView = new FlutterView(this)
+
+
+
     }
 
 }
