b35f41357b190637ff5bd404c208ae51abfbfe0c
fanjiajia <numen.fan@gmail.com>
2023-11-13 14:05:33 +0800

    测试merge commit 2

--- app/src/main/java/com/jiajia/mypractisedemos/BaseActivity.java
+++ app/src/main/java/com/jiajia/mypractisedemos/BaseActivity.java
@@ -14,6 +14,7 @@ import androidx.appcompat.app.AppCompatActivity;
  **/
 public abstract class BaseActivity extends AppCompatActivity {
 
+    private static final String TAG = "BaseActivity";
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
